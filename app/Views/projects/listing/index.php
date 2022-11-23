a833796055b1bdd0688ef7ca349c6e06330cbbab
Erik Gonzalez <erikgonzalez55@gmail.com>
2022-11-23 12:37:27 -0500

    Adicionando columnas de Municipio y Modo, boton de desabilitar(habilitar) en listado de proyectos

--- app/Views/projects/listing/index.php
+++ app/Views/projects/listing/index.php
@@ -13,6 +13,8 @@
 			<th scope="col"><?= lang('Globals.name') ?></th>
 			<th scope="col"><?= lang('Globals.city') ?></th>
 			<th scope="col"><?= lang('Globals.state') ?></th>
+			<th scope="col"><?= lang('Globals.municipality') ?></th>
+			<th scope="col"><?= lang('Globals.mode') ?></th>
 			<th scope="col" class="text-end"><?= lang('Globals.total') . " (" . lang('Globals.properties') . ")" ?></th>
 			<th scope="col" class="text-end"><?= lang('Globals.total') . " (" . lang('Globals.characteristics') . ")" ?></th>
 		</tr>
@@ -28,12 +30,17 @@
 								<li>
 									<a href="#" class="dropdown-item" data-bs-toggle="modal" data-bs-target="#addEditModal" data-modal-title="<?= lang('Globals.modify') ?>" data-uri-load-form="<?= base_url('projects/load_form_add_edit_listing') ?>" data-element-id="<?= $project->project_id ?>"><?= lang('Globals.modify') ?></a>
 								</li>
+								<li>
+									<a href="#" class="dropdown-item" data-bs-toggle="modal" data-bs-target="#deleteModal" data-modal-title="<?= lang('Globals.delete') ?>" data-uri-load-form="<?= base_url('projects/load_form_delete_listing') ?>" data-element-id="<?= $project->project_id ?>" data-element-name="<?= $project->project_name ?>" data-status-id="<?= $project->status_id ?>" data-menu-action="<?php if ($project->status_id == 1) echo lang('Globals.to disable'); else echo lang('Globals.enable'); ?>" ><?php if ($project->status_id == 1) echo lang('Globals.to disable'); else echo lang('Globals.enable'); ?></a>
+								</li>
 							</ul>
 						</div>
 					</td>
 					<td><?= $project->project_name ?></td>
 					<td><?= $project->city_name ?></td>
 					<td><?= $project->state_name ?></td>
+					<td><?= $project->municipality_name ?></td>
+					<td><?= $project->$statusNameField ?></td>
 					<td class="text-end"><?= $project->total_property ?></td>
 					<td class="text-end"><?= $project->total_amenity ?></td>
 				</tr>
@@ -61,6 +68,30 @@
 					<button type="button" class="btn btn-secondary" data-bs-dismiss="modal"><?= lang('Globals.close') ?></button>
 					<button type="submit" class="btn btn-primary"><?= lang('Globals.save') ?></button>
 				</div>
+			</form>
+		</div>
+	</div>
+</div>
+<div class="modal fade delete-modal" id="deleteModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-sm">
+		<div class="modal-content">
+			<form action="<?= base_url('projects/delete_listing') ?>" class="form-delete" method="POST">
+				<div class="modal-header">
+					<h5 class="modal-title" id="deleteModalLabel"></h5>
+					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="<?= lang('Globals.close') ?>"></button>
+				</div>
+				<div class="modal-body content">
+					<p>Está seguro de querer <span class="fs-5 fw-semibold" id="action_name"></span> el proyecto de <span class="fs-5 fw-semibold" id="project_name"></span></p>
+					<p class="mt-1 action-unable">Esto eliminará todas las propiedades (lotes) asociadas a éste.</p>
+
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-bs-dismiss="modal"><?= lang('Globals.close') ?></button>
+					<button type="submit" class="btn btn-danger" id="buttom_name"></button>
+					<!-- <button type="submit" class="btn btn-danger" id="buttom_name"></button> -->
+				</div>
+				<input type="hidden" name="project_id" id="project_id">
+				<input type="hidden" name="status_id" id="status_id">
 			</form>
 		</div>
 	</div>
