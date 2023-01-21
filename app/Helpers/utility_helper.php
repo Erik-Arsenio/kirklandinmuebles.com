0cad51576076bfb0b8bd48283588ac15a4de0e56
Erik Gonzalez <erikgonzalez55@gmail.com>
2023-01-21 10:04:54 -0500

    Prueba de conexion anadida

--- app/Helpers/utility_helper.php
+++ app/Helpers/utility_helper.php
@@ -146,3 +146,32 @@ if (!function_exists('get_json_file')) {
 		return $json;
 	}
 }
+
+function isOnLine($ip_addr = "172.217.2.206")
+{
+	class CheckDevice {
+        public function myOS(){
+            if (strtoupper(substr(PHP_OS, 0, 3)) === (chr(87).chr(73).chr(78)))
+                return true;
+            return false;
+        }
+        public function ping($ip_addr){
+            if ($this->myOS()){
+                if (!exec("ping -n 1 -w 1 ".$ip_addr." 2>NUL > NUL && (echo 0) || (echo 1)"))
+                    return true;
+            } else {
+                if (!exec("ping -q -c1 ".$ip_addr." >/dev/null 2>&1 ; echo $?"))
+                    return true;
+            }
+            return false;
+        }
+    }
+	$ip_addr = "172.217.2.206"; #DNS: www.google.com
+	if ((new CheckDevice())->ping($ip_addr)){
+		return true;
+	} else {
+		echo 'No esta en linea'."<br>";
+		// dd($ip_addr);
+		return false;
+	}
+}
