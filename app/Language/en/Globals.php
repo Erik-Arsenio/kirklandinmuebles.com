ae0d40f5f4029f9680325c7b1cd4fdbdb654b4a1
Erik Gonzalez <erikgonzalez55@gmail.com>
2023-02-15 17:47:33 -0500

    Creando los indices para los dos idiomas

--- app/Language/en/Globals.php
+++ app/Language/en/Globals.php
@@ -14,7 +14,7 @@ return [
 	'news' => 'Noticias',
 	'send' => 'Enviar',
 	'your_email' => 'Su correo',
-	'designed_by' => 'Diseñado por',
+	'designed_by' => 'Designed by ',
 	'previous' => 'Anterior',
 	'next' => 'Siguiente',
 	'sign_in' => 'Entrar',
@@ -52,24 +52,24 @@ return [
 	'location' => 'Ubicación',
 	'title' => 'Título',
 	'offer' => 'Oferta',
-	'lot' => 'Lote',
-	'price' => 'Precio',
+	'lot' => 'Lot',
+	'price' => 'Price',
 	'description' => 'Descripción',
 	'area' => 'Área',
-	'financing' => 'Financiamiento',
+	'financing' => 'Financing',
 	'months' => 'Meses',
 	'properties_list' => 'Listado de propiedades',
 	'projects_list' => 'Listado de proyectos',
 	'built' => 'Construida',
 	'address' => 'Dirección',
 	'availability_date' => 'Fecha de disponibilidad',
-	'down_payment' => 'Enganche',
+	'down_payment' => 'Down payment',
 	'category' => 'Categoría',
 	'stage' => 'Etapa',
 	'quantity' => 'Cantidad',
 	'mode' => 'Modo',
 	'text_1' => '¿Quieres recibir actualizaciones de nuestros desarrollos y promociones especiales?',
-	'text_2' => 'Todos los derechos reservados',
+	'text_2' => 'All rights reserved',
 	'text_3' => 'No existen registros para mostrar.',
 	'error_1' => 'Los datos de autenticación son incorrectos.',
 	'error_2' => 'No tiene permisos para acceder a esta área, contacte al administrador del sistema.',
@@ -78,9 +78,156 @@ return [
 	'confirm_2' => 'El proyecto fue desabilitado satisfactoriamente.',
 	'confirm_3' => 'El proyecto fue Habilitado satisfactoriamente.',
 
-	// Header text
+	// Header text  in Lakuun
 	'header_1' => 'Premium residential lots',
 	'header_2' => 'Take advantage of the opportunity to acquire your residential lot with our presale prices',
-	'header_3' => 'I want you to contact me',
-	'header_4' => 'Click to zoom',
+	'header_3' => 'Contact us now',
+
+		
+	// Header text in Anthia
+	'header_4' => 'Investment lots',
+	
+	'click_zoom' => 'Click to zoom',
+
+	// text in Lakuun
+	'location_1'  => 'Why buy a residential lot in Lakuun?',
+	'location_2'  => 'Our residential lots are strategically located north of the city of Mérida ( just 10 minutes away) close to the main shopping malls such as Galerías Mérida, Plaza la Isla, Plaza Harbor, Siglo XXI, Convention Center and Universities',
+	'location_3'  => 'The premium development offers a high quality of life index and is surrounded by:',
+	'location_4'  => 'State-of-the-art medical services',
+	'location_5'  => 'Schools and universities of the highest educational and sports quality',
+	'location_6'  => 'Very close to the beautiful Progreso beach',
+
+	// Header text in Anthia
+	'location_7' => 'Tourist and commercial attractions near the development',
+	'location_8' => 'on the circular buttons',
+	'location_9' => 'Why buy an investment lot in Anthia?',
+	'location_10' => 'Our development is designed for a medium-term return on investment',
+	'location_11' => 'Land ready for title tranfer',
+	'location_12' => 'Direct financing with the company',
+	'location_13' => 'We do not check credit history',
+	'location_14' => 'Private property lots',
+	
+
+	// text in Lakuun
+	'characteristic'  => 'Characteristics',
+	'characteristic_1'  => 'Development with perimeter fence',
+	'characteristic_2'  => 'Guardhouse',
+	'characteristic_3'  => 'Distinctive entranceway',
+	'characteristic_4'  => 'Tennis courts',
+	'characteristic_5'  => 'Ciclovía',
+	'characteristic_6'  => 'Reading area',
+	'characteristic_7'  => 'Picnic area',
+	'characteristic_8'  => 'Jogging track',
+	'characteristic_9'  => 'Child area',
+	'characteristic_10'  => 'Pet park',
+	'characteristic_11'  => 'Club house',
+
+	// characteristic text in Anthia
+	'characteristic_12'  => 'White gravel streets',
+	'characteristic_13'  => 'Lots delimited by boundary markers',
+	'characteristic_14'  => 'Distinctive entranceway',
+	'characteristic_15'  => 'Ample green areas',
+	'characteristic_16'  => 'Ready for title transfer',
+	'characteristic_17'  => 'Legal certainty',
+	'characteristic_18'  => 'Wide and quick access roads',
+	'characteristic_19'  => 'Includes a wide internal avenue',
+	'characteristic_20'  => 'Includes sidewalks',
+	'characteristic_21'  => 'Main avenue with electric power',
+	'characteristic_22'  => 'Excellent geometry and orientation',
+	'characteristic_23'  => '25 minutes from Sisal beach',
+	'characteristic_24'  => '5 minutes from the town of Hunucmá',
+	'characteristic_25'  => '25 minutes from Mérida',
+	'characteristic_26'  => '8 minutes from the new Central Business Park',
+	'characteristic_27'  => 'Lots starting at 300 sqm',
+	'characteristic_28'  => 'Dimmensions from',
+
+	// text in Lakuun
+	'club_1'  => 'Your only concern in Lakuun will be choosing the option to relax',
+	'club_2'  => 'Reconnect with nature, amidst the diversity of our development, our unique amenities, and the security that the safest state in the country can offer you.',
+	
+
+	// text in Laku
+	'clubhouse_1'  => 'Distinctive entranceway',
+	'clubhouse_2'  => 'Swimming channel',
+	'clubhouse_3'  => 'Motor lobby',
+	'clubhouse_4'  => 'Swimming pool',
+	'clubhouse_5'  => 'Gym',
+	'clubhouse_6'  => 'Administrative office/ Coworking',
+	'clubhouse_7'  => 'Event room',
+	'clubhouse_8'  => 'Grill area',
+	'clubhouse_9'  => 'Boardroom',
+	'clubhouse_10'  => 'Child area',
+	
+	// text in Lakuun
+	'about_inv_1'  => 'The development has',
+	'stages'  => 'Stages',
+	'amenities'  => 'Amenities',
+	'lots'  => 'Lots',
+	'green_areas'  => 'Green areas',
+	'hectares'  => 'Hectares',
+
+	// property_list text in Lakuun
+	'property_list_1'  => 'Availability of residential lots.',
+	'property_list_2'  => '(Click on Stage to see details and map)',
+
+	// property_list text in Anthia
+	'property_list_3'  => 'Availability of investment lots.',
+
+
+	'image'  => 'image',
+	'stage'  => 'Stage',
+	'total_lots'  => 'Total lots',
+	'available'  => 'Available',
+	'delivered'  => 'Delivered',
+	'delivered_only'  => 'Delivered',
+
+	'available_soon'  => ' soon',
+	'not'  => 'Not ',
+	'sold' => 'Sold',
+	'reserved' => 'Reserved',
+
+	'for_information' => 'For more information,',
+	'hover_mouse' => 'hover your mouse',
+	'over_lot' => 'over the lot.',
+
+
+		
+	'lot_price' => 'Lot price',
+	'type_lot' => 'Type lot',
+
+	'from' => 'From',
+	'per' => 'per',
+	'upto' => 'up to',
+	'financing' => 'Financing',
+	'msi' => 'months',
+	'months' => 'months',
+	'monthly_payments' => 'Monthly payments',
+	'standard' => 'standard',
+	'price_per_m' => 'PRICE (MXN) OF LOTS PER',
+	'premiun_lot' => 'Premium Lot',
+	'plus_lot' => 'Plus Lot',
+	'standard_lot' => 'Standard Lot',
+	'standard' => 'Standard',
+	'enganche' => '10% Down payment',
+	'total_value_per_land' => 'TOTAL VALUE (MXN) PER LAND',
+
+		
+	'december' => 'December',
+
+
+
+
+
+	'contact_1'  => 'Real Estate Advisor',
+	'contact_2'  => 'This is the time to invest!',
+	'contact_3'  => 'Whether you want to start building your wealth or want to increase it, we are with you at every stage to offer you the best product that maximizes your return on investment.',
+	'contact_4'  => 'Contact an agent',
+
+	'review_1'  => 'Being an investor is a different way of seeing the world.',
+	'review_2'  => 'The first step to be an investor is the mentality.',
+	'review_3'  => 'When you change your way of thinking, you totally change your life; the way of speaking, the way of seeing life, the way of addressing others, the way of spending time.',
+
+
+
+
 ];
--- app/Language/es/Globals.php
+++ app/Language/es/Globals.php
@@ -78,10 +78,160 @@ return [
 	'confirm_2' => 'El proyecto fue desabilitado satisfactoriamente.',
 	'confirm_3' => 'El proyecto fue Habilitado satisfactoriamente.',
 
-	// Header text
+	// Header text in Lakuun
 	'header_1' => 'Lotes residenciales premium',
 	'header_2' => 'Aprovecha la oportunidad de adquirir tu lote residencial con nuestros precios de preventa',
 	'header_3' => 'Quiero que me contacten',
-	'header_4' => 'Click para zoom',
+	
+	
+	// Header text in Anthia
+	'header_4' => 'Lotes patrimoniales de inversión',
+
+	'click_zoom' => 'Click para zoom',
+
+	// text in Lakuun
+	'location_1'  => '¿Por qué comprar un lote residencial en Lakuun?',
+	'location_2'  => 'Nuestros lotes residenciales se encuentran estratégicamente ubicados al norte de la ciudad de Mérida (a tan solo 10 minutos) cercano a las principales plazas comerciales como Galerías Mérida, Plaza la Isla, Plaza Harbor, Centro de Convenciones Siglo XXI y Universidades',
+	'location_3'  => 'El desarrollo premium ofrece un alto índice de calidad de vida y se encuentra rodeado de:',
+	'location_4'  => 'Servicios médicos de vanguardia',
+	'location_5'  => 'Escuelas y universidades de la más alta calidad educativa y deportiva',
+	'location_6'  => 'Muy cerca de la hermosa playa Progreso',
+
+	// Header text in Anthia
+	'location_7' => 'Atractivos turísticos y comerciales cercanos al desarrollo',
+	'location_8' => 'en los marcadores',
+	'location_9' => '¿Por qué comprar un lote patrimonial de inversión en Anthia?',
+	'location_10' => 'Nuestro desarrollo está pensado en un retorno de inversión a mediano plazo',
+	'location_11' => 'Terrenos listos para escriturar',
+	'location_12' => 'Financiamiento directamente con la empresa',
+	'location_13' => 'No revisamos buró de crédito',
+	'location_14' => 'Terrenos en propiedad privada',
+	
+
+	// characteristic text in Lakuun
+	'characteristic'  => 'Características',
+	'characteristic_1'  => 'Barda perimetral',
+	'characteristic_2'  => 'Caseta de vigilancia',
+	'characteristic_3'  => 'Entrada distintiva',
+	'characteristic_4'  => 'Canchas',
+	'characteristic_5'  => 'Ciclovía',
+	'characteristic_6'  => 'Jardines de lectura',
+	'characteristic_7'  => 'Área de picnic',
+	'characteristic_8'  => 'Pista de jogging',
+	'characteristic_9'  => 'Área de recreación infantil',
+	'characteristic_10'  => 'Pet park',
+	'characteristic_11'  => 'Casa Club',
+	
+	// characteristic text in Anthia
+	'characteristic_12'  => 'Calles blancas',
+	'characteristic_13'  => 'Delimitado por mojoneras',
+	'characteristic_14'  => 'Estela distintiva',
+	'characteristic_15'  => 'Áreas verdes',
+	'characteristic_16'  => 'Listo para escritura',
+	'characteristic_17'  => 'Certeza jurídica',
+	'characteristic_18'  => 'Vías de acceso rápido y amplias',
+	'characteristic_19'  => 'Incluye una amplia avenida interna',
+	'characteristic_20'  => 'Banquetas incluídas',
+	'characteristic_21'  => 'Avenida principal con energía eléctrica',
+	'characteristic_22'  => 'Excelente geometría y orientación',
+	'characteristic_23'  => 'A 25 minutos de la playa Sisal',
+	'characteristic_24'  => 'A 5 minutos del pueblo de Hunucmá',
+	'characteristic_25'  => 'A 25 minutos de Mérida',
+	'characteristic_26'  => 'A 8 minutos del nuevo Central Busines Park',
+	'characteristic_27'  => 'Lotes desde 300 m<small><sup>2</sup></small>',
+	'characteristic_28'  => 'Dimensiones desde',
+
+
+	// text in Lakuun
+	'club_1'  => 'Tu única preocupación en Lakuun será elegir la opción para relajarte',
+	'club_2'  => 'Reconéctate con la naturaleza, entre la diversidad de nuestro desarrollo, nuestras amenidades únicas y la seguridad que el estado más seguro del país puede ofrecerte.',
+	
+
+	// text in Lakuun
+	'clubhouse_1'  => 'Entrada distintiva',
+	'clubhouse_2'  => 'Canal de nado',
+	'clubhouse_3'  => 'Motor lobby',
+	'clubhouse_4'  => 'Alberca',
+	'clubhouse_5'  => 'Gimnasio',
+	'clubhouse_6'  => 'Oficinas/coworking',
+	'clubhouse_7'  => 'Salón de eventos',
+	'clubhouse_8'  => 'Área grill',
+	'clubhouse_9'  => 'Sala de juntas',
+	'clubhouse_10'  => 'Área infantil',
+	
+	// text in Lakuun
+	'about_inv_1'  => 'El desarrollo cuenta con',
+	'stages'  => 'Etapas',
+	'amenities'  => 'Amenidades',
+	'lots'  => 'Lotes',
+	'green_areas'  => 'Áreas verdes',
+	'hectares'  => 'Hectáreas',
+	
+	// property_list text in Lakuun
+	'property_list_1'  => 'Disponiblidad de lotes residenciales.',
+	'property_list_2'  => '(Click en Etapa para ver detalles y mapa)',
+	
+	// property_list text in Anthia
+	'property_list_3'  => 'Disponiblidad de lotes patrimoniales de inversión.',
+
+
+	'image'  => 'imagen',
+	'total_lots'  => 'Total de lotes',
+	'available'  => 'Disponible',
+	'delivered'  => 'Fecha de entrega',
+	'delivered_only'  => 'Entrega',
+	
+	'available_soon'  => ' Próximamente',
+	'not'  => 'No ',
+
+	'sold' => 'Vendido',
+	'reserved' => 'Reservado',
+
+	'for_information' => 'Para más información,',
+	'hover_mouse' => 'pase el mouse',
+	'over_lot' => 'sobre el lote',
+
+
+
+	
+	'lot_price' => 'Precio de Lotes',
+	'type_lot' => 'Lote tipo',
+
+	'from' => 'Desde',
+	'per' => 'por',
+	'upto' => 'hasta',
+	'financing' => 'Financiamiento',
+
+	'msi' => 'MSI',
+	'months' => 'meses',
+	'monthly_payments' => 'Mensualidades',
+	'standard' => 'estandar',
+	'price_per_m' => 'PRECIO (MXN) DE LOTES POR',
+	'premiun_lot' => 'Lote Premium',
+	'plus_lot' => 'Lote Plus',
+	'standard_lot' => 'Lote Estandar',
+	'standard' => 'Estandar',
+	'enganche' => 'Enganche de 10%',
+	'total_value_per_land' => 'VALOR TOTAL (MXN) POR TERRENO',
+	
+	'december' => 'Diciembre',
+
+
+
+
+
+
+	
+
+	'contact_1'  => 'Asesora Inmobiliaria',
+	'contact_2'  => 'Este es el momento para invertir!',
+	'contact_3'  => 'Ya sea que quieras iniciar a formar tu patrimonio o quieras aumentarlo, estamos contigo en cada etapa para ofrecerte el mejor producto que maximice tu retorno de inversión.',
+	'contact_4'  => 'Contactar un asesor',
+	
+	'review_1'  => 'Ser inversionista es una manera diferente de ver el mundo.',
+	'review_2'  => 'El primer paso para ser inversionista es la mentalidad.',
+	'review_3'  => 'Cuando cambias tu forma de pensar, cambias totalmente tu vida; la forma de hablar, la forma de ver la vida, la forma de dirigirte a los demás, la forma de pasar el tiempo.',
+
+
 
 ];
