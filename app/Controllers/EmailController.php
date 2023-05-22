402ac431059be083cab39475fd3d4a75de26aa9d
Erik Gonzalez <erikgonzalez55@gmail.com>
2023-05-22 17:50:50 -0400

    Nuevo desarrollo de Life y rectificando idioma a recibir del contacto y manejo del codigo de promocion

--- app/Controllers/EmailController.php
+++ app/Controllers/EmailController.php
@@ -21,7 +21,8 @@ class EmailController extends BaseController
 
 	public function index()
 	{
-        $projects = array('Anthia', 'Lakuun', 'Marela Beach', 'Marela Celestun');
+        $projects = array('Anthia', 'Lakuun', 'Marela Life', 'Marela Beach');
+        $lang_return = array('Spanish', 'English', 'Either');
         // $getSort = 'country_phone_prefix ASC';
         // $builder = $this->settings->getCountries(null, true, 'country_phone_prefix ASC');
         // $phone_prefix = array_column($builder, 'country_phone_prefix');
@@ -39,6 +40,7 @@ class EmailController extends BaseController
 		
 		// echo "Leguaje del select- " . $lang;
         $dataContact = [
+            'lang_return' => $lang_return,
             'projects' => $projects,
             'lang' => $lang
             // 'country_phone_prefix' => $country_phone_prefix
@@ -64,10 +66,13 @@ class EmailController extends BaseController
             // 'country_phone_prefix' => 'required',
             'phone'=>'required|alpha_numeric_punct|min_length[8]|max_length[22]',
             'email'=>'required|valid_email',
+            'code_promo'=>'permit_empty',
             'message'=>'permit_empty',
-            'project[]'=>'permit_empty'
+            'project[]'=>'permit_empty',
+            'lang_return[]'=>'permit_empty',
             
         ]);
+        $promo_code = array("YEF2kx492vuM", "MIG3sRMGYZgo");
         $lang = $this->request->getVar('lang');
         if (!$validation->withRequest($this->request)->run()){
             // dd($this->request->getPost());
@@ -83,8 +88,10 @@ class EmailController extends BaseController
                 // $phone_prefix = $this->request->getVar('country_phone_prefix');
                 $phone = $this->request->getVar('phone');
                 $mail = $this->request->getVar('email');
+                $code_promo = $this->request->getVar('code_promo');
                 $subject = lang('Globals.subject', [], $lang) ; 
-                $message = '<b>' . lang('Globals.contactform_4', [], $lang) . ':</b> '.$name. ' '.$last_name.'<br>'.'<b>' . lang('Globals.contactform_6', [], $lang) . ':</b> '.$phone.'<br>'.'<b>' . lang('Globals.contactform_7', [], $lang) . ':</b> '.$mail;
+                $message = '<b>' . lang('Globals.contactform_4', [], $lang) . ':</b> '.$name. ' '.$last_name.'<br>'.'<b>' . lang('Globals.contactform_6', [], $lang) 
+                    . ':</b> '.$phone.'<br>'.'<b>' . lang('Globals.contactform_7', [], $lang) . ':</b> '.$mail. '<br>' . '<b>' . lang('Globals.contactform_16', [], $lang) . ':</b> '.$code_promo;
                 $doubts = $this->request->getVar('message');
                 if($doubts != ''){
                     $message = $message.'<br><br>'.'<b>' . lang('Globals.doubts', [], $lang) . ':</b> '.'<br>'.$doubts;
@@ -110,20 +117,37 @@ class EmailController extends BaseController
                 // Las comunicaciones prefiero recibirlas en
                 //  I prefer to receive communications in
 
-                $spanish = $this->request->getVar('spanish');
-                $english = $this->request->getVar('english');
-                $either = $this->request->getVar('either');
-
-                if (isset($_POST['spanish']) || isset($_POST['english']) || isset($_POST['either'])) {
-                    $language = 'Si';
-                    $message = $message.'<br><br>'.'<b>' . lang('Globals.language_info', [], $lang) . ':</b> ';
-                    if (isset($_POST['either'])) {
-                        $message = $message. $either . ' ' . strtolower(lang('Globals.language', [], $lang)) ;
-                    } else if (isset($_POST['spanish']) || isset($_POST['english']) ) {
-                        isset($_POST['spanish']) ? $message = $message. $spanish . ", " : '';
-                        isset($_POST['english']) ? $message = $message. $english : '';
+                if (isset($_POST['lang_return']) && is_array($_POST['lang_return'])) {
+                    $selected_lang_return = '';
+                    $num_lang_return = count($_POST['lang_return']);
+                    $current = 0;
+                    foreach ($_POST['lang_return'] as $key => $value) {
+                        if ($current != $num_lang_return-1)
+                            $selected_lang_return .= $value.', ';
+                        else
+                            $selected_lang_return .= $value.'.';
+                        $current++;
                     }
-                } 
+                    if($num_lang_return >= 1){
+                        $message = $message.'<br><br>'.'<b>' . lang('Globals.language_info', [], $lang) . ':</b> '.$selected_lang_return;
+                    }
+                }
+
+                // $spanish = $this->request->getVar('spanish');
+                // $english = $this->request->getVar('english');
+                // $either = $this->request->getVar('either');
+  
+                // if (isset($_POST['spanish']) || isset($_POST['english']) || isset($_POST['either'])) {
+                //     $language = 'Si';
+                //     $message = $message.'<br><br>'.'<b>' . lang('Globals.language_info', [], $lang) . ':</b> ';
+                //     if (isset($_POST['either'])) {
+                //         $message = $message. $either . ' ' . strtolower(lang('Globals.language', [], $lang)) ;
+                //     } else if (isset($_POST['spanish']) || isset($_POST['english']) ) {
+                //         isset($_POST['spanish']) ? $message = $message. $spanish . ", " : '';
+                //         isset($_POST['english']) ? $message = $message. $english : '';
+                //     }
+                // } 
+
                 // else {
                 //     $language = 'No';
                 // }
@@ -134,16 +158,24 @@ class EmailController extends BaseController
                 // $new_message = $view->setVar('message', $message)->render('email/email_template');
 
 
+                if ($code_promo == "MIG3sRMGYZgo") {
+                    $email->setTo('michel@kirklandinmobiliaria.com');
+                    $email->setCC('carmen@kirklandinmobiliaria.com');
+                    $email->setBCC('contact@kirklandinmobiliaria.com');
+                } else if ($code_promo == "YEF2kx492vuM" || $code_promo == "") {
+                    $email->setTo('carmen@kirklandinmobiliaria.com');
+                    $email->setCC('contact@kirklandinmobiliaria.com');
+                    // $email->setBCC('carmenphasesorainmobiliaria@gmail.com');
+                    $email->setBCC('erikgonzalez55@kirklandinmobiliaria.com');
+                }
+
                 // $email->setTo($to);
-                $email->setTo('carmen@kirklandinmobiliaria.com');
-                $email->setCC('contact@kirklandinmobiliaria.com');
-                // $email->setBCC('carmenphasesorainmobiliaria@gmail.com');
-                $email->setTo('erikgonzalez55@kirklandinmobiliaria.com');
                 $email->setFrom($this->request->getVar('email'), $this->request->getVar('name'));
                 $email->setSubject($subject);
                 $email->setMessage($message);
             
                 // var_dump($spanish);
+                // dd($promo_code, $lang, $code_promo, $message, $email);
                 // die;
                 if($email->send()){
                     return redirect()->to('templates/contactForm?lang=' . $lang)->with('success', lang('Globals.send_success', [], $lang));
