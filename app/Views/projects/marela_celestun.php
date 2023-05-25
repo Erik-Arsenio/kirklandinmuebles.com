52994e0289d9d395033558521d87f5ba46573642
Erik Gonzalez <erikgonzalez55@gmail.com>
2023-05-25 14:04:23 -0400

    Rectificando nombre de  wind-rose para Marela Celestun

--- app/Views/projects/marela_celestun.php
+++ app/Views/projects/marela_celestun.php
@@ -4539,7 +4539,7 @@
                                 Loading image...
                             </div>
                             <div class="row">
-                                <img src="<?= STATIC_URL . 'img/wind-rose_ES.svg' ?>" alt="Brujula" height="80px" width="80px" style="transform: rotate(0deg);">
+                                <img src="<?= STATIC_URL . 'img/wind-rose_es.svg' ?>" alt="Brujula" height="80px" width="80px" style="transform: rotate(0deg);">
                             </div>
                             <div class="row mt-2">
                                 <p class="small fw-bolder text-center">Para más información, pase el mouse sobre el mapa</p>
